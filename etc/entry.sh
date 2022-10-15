34af8b0896406b3e7b8870526b03e203f5c674e8
jobggun <jobggun@kakao.com>
2022-10-15 15:05:06 +0900

    added beta

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,7 +3,7 @@ mkdir -p "${STEAMAPPDIR}" || true
 
 bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
                 +login anonymous \
-                +app_update "${STEAMAPPID}" \
+                +app_update "${STEAMAPPID}${STEAMAPPBETA:+' -beta '}${STEAMAPPBETA}" \
                 +quit
 
 # Believe it or not, if you don't do this srcds_run shits itself
